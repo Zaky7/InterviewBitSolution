17d3f50728a7c079c570b30238bea6cade8e495f
Zaky7 <zak.saifi7@gmail.com>
2019-06-02 13:21:44 +0530

    First Commit

---
+++ src/Arrays/LargestNumber.java
@@ -0,0 +1,69 @@
+package Arrays;/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
+
+class LargestNumber {
+	public static void main (String[] args){
+	      ArrayList<Integer> arr = new ArrayList<Integer>();
+	      arr.add(49);
+	      arr.add(37);
+	      arr.add(39);
+	      arr.add(12);
+
+
+        LargestNumber gfs = new LargestNumber();
+	      System.out.println(gfs.findNthPlace(49, 2));
+	      System.out.println(gfs.findDigits(485));
+	   //   gfs.printArr(arr);
+	}
+	
+	void printArr(ArrayList<Integer> arr) {
+	    System.out.println("Array Elements are: ");
+	    for(Integer ele: arr) {
+	        System.out.print(ele + " ");
+	    }
+	    System.out.println();
+	}
+   
+    int findMaxList(List<Integer> list) {
+        int size = list.size();
+        int result = 0;
+        
+        if(size > 0) {
+            result = list.get(0);
+            for(int i=1; i< size; i++) {
+                if(result < list.get(i)) {
+                    result = list.get(i);
+                }
+            }
+        }
+        
+        return result;
+    }
+    
+    int findDigits(int num) {
+        int digits = 0;
+        while(num >= 0) {
+           digits++;
+           num = num / 10;
+        }
+        return digits;
+    }
+    
+    int findNthPlace(int num, int pos) {
+        for(int i=0; i < pos - 1; pos--) {
+            // System.out.println("Num: " + num + " Pos: " + pos);
+            num = num / 10;
+        }
+        return num % 10;
+    }
+
+   
+    // String reverseRadixSort(final List<Integer> list) {
+        
+    //     int maxDigits = findDigits(findMaxList(list));
+    // }
+}
---
+++ src/String/PallindromeSubString.java
@@ -0,0 +1,63 @@
+package String;
+
+public class PallindromeSubString {
+
+	public String longestPalindrome(String s) {
+
+		int n = s.length();
+		int STRING_MAX_LEN = Integer.MIN_VALUE;
+		String maxlen_substring = Character.toString(s.charAt(0));
+		
+		// Find all the substring and check if pallindrome
+		for (int size = n-1; size >= 0; size--) {
+			for (int start = 0; start < n; start++) {
+//				System.out.println("Start: " + start + " Size: " + size);
+				if (size + start > n) {
+					break;
+				}
+
+				StringBuilder sb = new StringBuilder();
+				for (int k = start; k <= size; k++) {
+					sb.append(s.charAt(k));
+				}
+
+				String subString = sb.toString();
+//				System.out.println("String: " + subString);
+				if (isPallindrome(subString)) {
+					System.out.println("Pallindrome String: " + subString);
+					if (subString.length() > STRING_MAX_LEN) {
+						maxlen_substring = subString;
+						STRING_MAX_LEN = maxlen_substring.length();
+					}
+				}
+			}
+
+		}
+		
+		return maxlen_substring;
+	}
+
+	public boolean isPallindrome(String str) {
+		char[] chArray = str.toCharArray();
+		int n = str.length();
+		int i = 0, j = n - 1;
+		boolean pallindrome = true;
+
+		while (i < j) {
+			if (chArray[i] != chArray[j]) {
+				pallindrome = false;
+				break;
+			}
+			i++;
+			j--;
+		}
+
+		return pallindrome;
+	}
+
+	public static void main(String args[]) {
+		PallindromeSubString psb = new PallindromeSubString();
+		
+		System.out.println(psb.longestPalindrome("babaabc"));
+	}
+}
